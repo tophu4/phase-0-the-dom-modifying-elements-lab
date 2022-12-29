4a1d8abfb3356630e613536a35ce3df714523aac
Christopher Dean Canlas <chrisdcanlas@gmail.com>
2022-12-29 05:09:26 -0800

    commit tree a54acb203da53c1c482bebb6dbc9186005bf3ca5

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+const main = document.getElementById('main');
+main.remove();
+
+const newHeader = document.createElement('h1');
+const text = document.createTextNode("Chris is the champion");
+newHeader.setAttribute('id','victory');
+newHeader.appendChild(text);
+document.body.appendC(newHeader);
